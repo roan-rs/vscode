5ffa131254538dccbbd8751fc970e59a9dcc3278
malezjaa <poreba.kris@gmail.com>
2024-11-03 22:39:16 +0100

    feat: show diagnostic in the right place

--- server/src/on_change.rs
+++ server/src/on_change.rs
@@ -2,9 +2,9 @@ use crate::Backend;
 use percent_encoding::percent_decode;
 use roan_engine::module::Module;
 use roan_engine::source::Source;
-use roan_error::error::RoanError;
+use roan_error::error::{get_span_from_err, RoanError};
 use ropey::Rope;
-use tower_lsp::lsp_types::{Diagnostic, DiagnosticSeverity, MessageType, TextDocumentItem};
+use tower_lsp::lsp_types::{Diagnostic, DiagnosticSeverity, MessageType, Position, Range, TextDocumentItem};
 
 impl Backend {
     pub async fn on_change(&self, doc: TextDocumentItem, source: Source) {
@@ -15,17 +15,39 @@ impl Backend {
             .insert(doc.uri.to_string(), Rope::from(source.content()));
 
         match module.parse() {
-            Ok(..) => {}
+            Ok(..) => {
+                self.client.publish_diagnostics(doc.uri.clone(), vec![], Some(doc.version)).await;
+            }
             Err(err) => {
                 self.client
                     .log_message(MessageType::ERROR, format!("Error: {}", err))
                     .await;
-                let diag = Diagnostic {
+
+                let mut diag = Diagnostic {
                     range: Default::default(),
                     severity: Some(DiagnosticSeverity::ERROR),
                     message: err.to_string(),
                     ..Default::default()
                 };
+
+                if let Some(err) = err.downcast_ref::<RoanError>() {
+                    let span = get_span_from_err(err);
+
+                    if let Some(span) = span {
+                        let range = Range {
+                            start: Position {
+                                line: span.start.line,
+                                character: span.start.column,
+                            },
+                            end: Position {
+                                line: span.end.line,
+                                character: span.end.column,
+                            },
+                        };
+
+                        diag.range = range;
+                    }
+                }
 
                 self.client
                     .publish_diagnostics(doc.uri.clone(), vec![diag], Some(doc.version))
